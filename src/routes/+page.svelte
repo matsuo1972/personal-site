2716e96177f98a8921cb78087aae2409d3309369
matsuo <matsuo.freelance@gmail.com>
2023-06-23 13:05:21 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script> 
+    export let data;
+</script>
+
+<div class="row">
+    <div class="col">
+        <h1>Noah's wesome Website</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <p>This is my website. Thank you for checking it out!</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <h1>Daily Quote</h1>
+        <p>{ data.quote }</p>
+        <p>{ data.author }</p>
+    </div>
+</div>
